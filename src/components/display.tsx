b5edde4cc814f6c1f742d805fa180000d47c5d5e
Dagadev <gacub20ru@gmail.com>
2023-09-22 21:55:30 -0600

    add display component

---
+++ src/components/display.tsx
@@ -0,0 +1,31 @@
+import { View, Text } from 'react-native'
+import useCalculo from '../hooks/use-calculo'
+import { colors } from '../styles/colors'
+import CustomText from './custom-text'
+
+export default function Display () {
+  const { targetNumber, previousNumber, operation, result, finalResult } = useCalculo()
+
+  console.log(finalResult)
+
+  return (
+    <View style={{
+      flex: 2,
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end'
+    }}>
+      {(previousNumber !== undefined && operation !== undefined) && <Text style={{
+        color: colors.gray,
+        fontSize: 30,
+        lineHeight: 30
+      }}>{previousNumber.toLocaleString()} <Text style={{ color: colors.result }}>{operation}</Text></Text>}
+
+      {/* {(previousNumber !== undefined && operation !== undefined) && <CustomText type='md' content={previousNumber.toLocaleString()}><Text style={{ color: colors.result }}>{operation}</Text></CustomText>} */}
+      <CustomText type={finalResult === undefined ? 'principal' : 'result'} content={finalResult === undefined
+        ? targetNumber.toLocaleString()
+        : finalResult.toLocaleString()
+      } />
+      {result !== undefined && <CustomText type='sm' content={result.toLocaleString()} />}
+    </View>
+  )
+}
