69ba745ce22cc1b235ee2a623d11730b9eb58bff
Dagadev <gacub20ru@gmail.com>
2023-09-22 21:53:41 -0600

    add calculo provider & add new components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, View, Text, SafeAreaView, Switch } from 'react-native'
+import { StyleSheet, View, SafeAreaView, Switch } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { useState } from 'react'
 import { colors } from './src/styles/colors'
 import { ThemeContext } from './src/contexts/theme-context'
 import type { ToggleTheme } from './src/utils/types'
-import MyKeyboard from './src/components/my-keyboard'
+import Keyboard from './src/components/keyboard'
+import Display from './src/components/display'
+import CalculoProvider from './src/providers/calculo-provider'
 
 export default function App () {
   const [theme, setTheme] = useState<ToggleTheme>('light')
@@ -13,8 +15,13 @@ export default function App () {
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, { backgroundColor: colors.dark }]}>
         <StatusBar style={theme === 'light' ? 'dark' : 'light'} />
-        <Switch value={theme === 'light'} onValueChange={() => { setTheme(t => t === 'light' ? 'dark' : 'light') }} />
-        <MyKeyboard />
+        <Switch value={theme === 'dark'} onValueChange={() => { setTheme(t => t === 'light' ? 'dark' : 'light') }} />
+        <View style={{ flex: 1 }}>
+          <CalculoProvider>
+            <Display />
+            <Keyboard />
+          </CalculoProvider>
+        </View>
       </SafeAreaView>
     </ThemeContext.Provider>
   )
@@ -23,6 +30,7 @@ export default function App () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'space-between',
     backgroundColor: colors.light
   }
